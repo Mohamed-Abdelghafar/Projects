7eb7707c93ddb8272503c9a03e12d0427d8dbd31
Mohamed-Abdelghafar <mohammedabdelghafar2030@gmail.com>
2023-12-11 18:22:48 +0200

    more quotes added

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-var mohamed = -1; // 2
-var Owners = ["الإمام الشافعي", "عمر بن الخطاب", "عبدالله بن عمر"]
-var Quotes = [" العاقل إذا أخطأ يتأسف - والأحمق إذا أخطأ يتفلسف",
-    "ما ندمت علي سكوتي مره - لكنني ندمت علي الكلام مراراً",
-    "أحب الناس الي الله أنفعهم للناس"]
+var mohamed = -1;
+var Owners = ["الحسن البصري", "عمر بن عبد العزيز", "الإمام الشافعي", "عمر بن الخطاب", "عبدالله بن عمر"]
+var Quotes = [" . العاقل إذا أخطأ يتأسف - والأحمق إذا أخطأ يتفلسف",
+    ". ما ندمت علي سكوتي مره - لكنني ندمت علي الكلام مراراً",
+    ". أحب الناس الي الله أنفعهم للناس",
+    ". أصلحوا آخرتكم تصلح لكم دنياكم ",
+    ". الصبر كنز من كنوز الجنة ، وإنما يدرك الإنسان الخير كله بصبر ساعة"
+]
 
 function randomNum() {
     do { var num = Math.floor(Math.random() * Owners.length) }
-    while (num == mohamed) // 2 == -1 // 3
+    while (num == mohamed) 
     mohamed = num; 
     document.getElementById("Quote-text").innerHTML = Quotes[num];
     document.getElementById("Quote-owner").innerHTML = Owners[num];
